77d4165b3be35c3d737b24a996f5a27bc8d0d797
AzureAD\KennethJohnMatos <kmatos@yotpo.com>
2025-04-30 21:12:50 +0800

    BedreNaetter.dk Widgets 3.0

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "712000";
+  let InstanceId_Widget = "838740";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "711998";
+  let InstanceId_StarRating = "1101003";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "689569544";
+  let ProductId = "9149";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
